c8c27d299a274122cdc9090892983aaa7686e053
bherdwajneha3001 <bherdwajneha3001@gmail.com>
2024-02-16 12:54:33 +0530

    folder structure modified

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
+import React from 'react';
 import './App.css';
-import Modal from './components/Modal';
-import LargeBookListItem from './components/books/LargeListItem';
-import { books } from './components/data/books';
-import Lists from './components/lists';
-import SplitScreen from './components/split-screen';
-
-const Left = ({ title }) => {
-  return <h2 style={{ backgroundColor: 'palegreen' }}>{title}</h2>;
-};
-
-const Right = ({ title }) => {
-  return <h2 style={{ backgroundColor: 'blanchedalmond' }}>{title}</h2>;
-};
+import LayoutComponent from './components/LayoutComponents';
 
 function App() {
   return (
     <>
-      <SplitScreen
-        // Left={Left}       // components passed as props
-        // Right={Right}
-        leftCompWidth={1}
-        rightCompWidth={2}
-      >
-        {/* passing components as children which is good practice */}
-        <Left title='Left' />
-        <Right title='Right' />
-      </SplitScreen>
-
-      <Modal>
-        <LargeBookListItem books={books[0]} />
-      </Modal>
-
-      <Lists />
+      <LayoutComponent />
     </>
   );
 }
---
+++ src/components/LayoutComponents/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Modal from './Modal';
+import LargeBookListItem from './books/LargeListItem';
+import SplitScreen from './split-screen';
+import { books } from '../LayoutComponents/data/books';
+import Lists from './lists';
+
+const Left = ({ title }) => {
+  return <h2 style={{ backgroundColor: 'palegreen' }}>{title}</h2>;
+};
+
+const Right = ({ title }) => {
+  return <h2 style={{ backgroundColor: 'blanchedalmond' }}>{title}</h2>;
+};
+
+function LayoutComponent() {
+  return (
+    <>
+      <SplitScreen
+        // Left={Left}       // components passed as props
+        // Right={Right}
+        leftCompWidth={1}
+        rightCompWidth={2}
+      >
+        {/* passing components as children which is good practice */}
+        <Left title='Left' />
+        <Right title='Right' />
+      </SplitScreen>
+
+      <Modal>
+        <LargeBookListItem books={books[0]} />
+      </Modal>
+
+      <Lists />
+    </>
+  );
+}
+
+export default LayoutComponent;
